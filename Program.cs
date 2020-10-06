78a06dad34fbdea375dd20a638aa6292883ad2bf
Aseem <aseemanand2@gmail.com>
2020-10-06 15:01:37 +0530

    [Aseem].[Add] Code to validate first name

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ namespace UserRegistrationProblem
     {
         static void Main(string[] args)
         {
+            ValidEntry validEntry = new ValidEntry();
             Console.WriteLine("Welcome to User registration portal!");
-
+            //Validation for first name
+            validEntry.validateFirstName();
         }
     }
 }
---
+++ ValidEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    class ValidEntry
+    {
+        private readonly NLog nLog = new NLog();
+        public void validateFirstName()
+        {
+            //Pattern for valid first name
+            string firstNamePattern = @"^[A-Z]{1}([a-zA-Z]+){2,}";          
+            Regex regex = new Regex(firstNamePattern);
+            //User input
+            Console.WriteLine("Enter the first name of the user");
+            string firstName = Console.ReadLine();
+            //IF valid first name
+            if(regex.IsMatch(firstName))
+            {
+                Console.WriteLine("Thank you for entering a valid first name");
+                nLog.LogInfo("User entered a valid first name: "+firstName);
+            }  
+            //If invalid first name
+            else
+            {
+                Console.WriteLine("Invalid first name, must have atleast 3 characters and first letter must be capital");
+                nLog.LogError("User entered an invalid first name: "+firstName);
+            }
+        }
+    }
+}
