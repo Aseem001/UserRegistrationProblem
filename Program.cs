492c693c14e327afa642631ec5b86d3d39b69948
Aseem <aseemanand2@gmail.com>
2020-10-06 19:35:59 +0530

    [Aseem].[Add] Validate if the password has atleast one numeral

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace UserRegistrationProblem
     class Program
     {
         static void Main(string[] args)
-        {           
+        {
             ValidEntry validEntry = new ValidEntry();
             Console.WriteLine("Welcome to User registration portal!");
             ////Validation for first name
