7db6ec855be10e4e720fed9defb13e20d2479bfd
Aseem <aseemanand2@gmail.com>
2020-10-13 12:13:50 +0530

    [Aseem] Added MSTests to validate user details

---
+++ MSTestForUserRegistration/UnitTest1.cs
@@ -0,0 +1,81 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegistrationProblem;
+
+namespace MSTestForUserRegistration
+{
+    [TestClass]
+    public class UnitTest1
+    {       
+        [TestMethod]
+        [DataRow("Virat")]
+        [DataRow("kojhli")]
+        public void ValidateNameTest(string name)
+        {
+            ValidEntry validEntry = new ValidEntry();
+            string firstOrLastNamePattern = @"^[A-Z]{1}[a-zA-Z]{2,}$";
+            try
+            {
+                bool actual = validEntry.IsValid(firstOrLastNamePattern, name);
+                Assert.AreEqual(true, actual);
+            }
+            catch(UserRegistrationCustomException e)
+            {
+                Assert.AreEqual("Exception: Invalid Details Entered", e.Message);
+            }
+        }
+
+        [TestMethod]
+        [DataRow("abc@abc@gmail.com")]
+        [DataRow("abc.100@abc.com.au")]
+        public void ValidateEmailTest(string name)
+        {
+            ValidEntry validEntry = new ValidEntry();
+            string emailPattern = @"^[a-zA-Z0-9]+([.+_-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{3})+([.][a-zA-Z]{2})?$";
+            try
+            {
+                bool actual = validEntry.IsValid(emailPattern, name);
+                Assert.AreEqual(true, actual);
+            }
+            catch (UserRegistrationCustomException e)
+            {
+                Assert.AreEqual("Exception: Invalid Details Entered", e.Message);
+            }
+        }
+
+        [TestMethod]
+        [DataRow("91 4566784567")]
+        [DataRow("47646873 80")]
+        public void ValidateMobileNumberTest(string name)
+        {
+            ValidEntry validEntry = new ValidEntry();
+            string mobileNumberPattern = @"^[0-9]{2}[ ][0-9]{10}$";
+            try
+            {
+                bool actual = validEntry.IsValid(mobileNumberPattern, name);
+                Assert.AreEqual(true, actual);
+            }
+            catch (UserRegistrationCustomException e)
+            {
+                Assert.AreEqual("Exception: Invalid Details Entered", e.Message);
+            }
+        }
+
+        [TestMethod]
+        [DataRow("hcAggfG13436@")]
+        [DataRow("gchgcL#4675@@")]
+        public void ValidatePasswordTest(string name)
+        {
+            ValidEntry validEntry = new ValidEntry();
+            string passwordPattern = @"^(?=.*[A-Z])(?=.*[0-9])(?=.*[^0-9a-zA-Z])(?!.*[^0-9a-zA-Z].*[^0-9a-zA-Z]).{8,}$";
+            try
+            {
+                bool actual = validEntry.IsValid(passwordPattern, name);
+                Assert.AreEqual(true, actual);
+            }
+            catch (UserRegistrationCustomException e)
+            {
+                Assert.AreEqual("Exception: Invalid Details Entered", e.Message);
+            }
+        }
+    }
+}
